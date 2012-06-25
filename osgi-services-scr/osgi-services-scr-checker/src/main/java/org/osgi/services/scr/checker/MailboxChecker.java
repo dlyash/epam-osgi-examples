60196d395d1a7ae503533225bddd1ee9107ca719
Dmytro Lyash <Dmytro_Lyash@epam.com>
2012-06-25 13:52:54 +0300

    We do not need cleanup code - container will recteate component on config change

--- osgi-services-scr/osgi-services-scr-checker/src/main/java/org/osgi/services/scr/checker/MailboxChecker.java
+++ osgi-services-scr/osgi-services-scr-checker/src/main/java/org/osgi/services/scr/checker/MailboxChecker.java
@@ -5,9 +5,7 @@ import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.osgi.mailboxes.api.Mailbox;
 
@@ -18,7 +16,6 @@ public class MailboxChecker {
 	private long period = DEFAULT_SCAN_PERIOD;
 	private final List<Mailbox> mailboxes = new CopyOnWriteArrayList<Mailbox>();
 	private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-	private final AtomicReference<ScheduledFuture<?>> runningTask = new AtomicReference<ScheduledFuture<?>>();;
 	
 	private class CheckerTask implements Runnable {
 		@Override
@@ -47,11 +44,7 @@ public class MailboxChecker {
 		
 		System.out.println("Activating component. Period = " + period);
 		
-		ScheduledFuture<?> newTask = scheduler.scheduleWithFixedDelay(new CheckerTask(), period, period, TimeUnit.MILLISECONDS);
-		ScheduledFuture<?> oldTask = runningTask.getAndSet(newTask);
-		if (oldTask != null) {
-			oldTask.cancel(true);
-		}
+		scheduler.scheduleWithFixedDelay(new CheckerTask(), period, period, TimeUnit.MILLISECONDS);
 	}
 	
 	protected void deactivate() {
